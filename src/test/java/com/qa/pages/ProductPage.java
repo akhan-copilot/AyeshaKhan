ad4cb6809b4a650c9dc896db458918bf538b8a4c
Ayesha Khan <akhan@corcentric.com>
2024-12-22 23:16:43 +0500

    final commit

--- src/test/java/com/qa/pages/ProductPage.java
+++ src/test/java/com/qa/pages/ProductPage.java
@@ -1,4 +1,50 @@
 package com.qa.pages;
 
-public class ProductPage {
+import com.qa.base.AppDriver;
+import com.qa.base.AppFactory;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage extends AppFactory {
+
+    public ProductPage() {
+        PageFactory.initElements(new AppiumFieldDecorator(AppDriver.getDriver()), this);
+    }
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text=\"PRODUCTS\"]")
+    public WebElement productHeader;
+
+    public String getTitle(){
+        return getAttribute(productHeader, "text");
+    }
+
+    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"test-Menu\"]/android.view.ViewGroup/android.widget.ImageView")
+    public WebElement sideNavigationBar;
+
+    public void clickSideNavigationBar(){
+        clickElement(sideNavigationBar, "Clicking SideNavigation Button");
+    }
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text=\"LOGOUT\"]")
+    public WebElement logoutBtn;
+
+    public void clickLogout(){
+        clickElement(logoutBtn, "Clicking Logout Button");
+    }
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@content-desc=\"test-Item title\" and @text=\"Sauce Labs Backpack\"]")
+    public WebElement productImage;
+
+    public void clickProductImage(){
+        clickElement(productImage, "Clicking product Image");
+    }
+
+    @AndroidFindBy(accessibility = "test-BACK TO PRODUCTS")
+    public  WebElement backToProductsBtn;
+
+    public boolean isBackToProductsBtnDisplayed(){
+        return backToProductsBtn.isDisplayed();
+    }
 }
--- src/test/java/com/qa/testcases/LoginTest.java
+++ src/test/java/com/qa/testcases/LoginTest.java
@@ -1,4 +1,110 @@
 package com.qa.testcases;
 
-public class LoginTest {
+import com.aventstack.extentreports.Status;
+import com.qa.base.AppFactory;
+import com.qa.pages.LoginPage;
+import com.qa.pages.ProductPage;
+import com.qa.reports.ExtentReport;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+
+public class LoginTest extends AppFactory {
+
+    LoginPage loginPage;
+    ProductPage productPage;
+
+    InputStream inputStream;
+
+    JSONObject loginUser;
+
+
+    @BeforeMethod
+    public void setup(Method method) {
+        loginPage = new LoginPage();
+        utilities.log().info("\n\n********** " + method.getName() + " **********\n\n");
+    }
+
+    @BeforeClass
+    public void setupDataStream() throws IOException {
+        try {
+            String dataFileName = "data/loginUser.json";
+            inputStream = getClass().getClassLoader().getResourceAsStream(dataFileName);
+            JSONTokener jsonTokener = new JSONTokener(Objects.requireNonNull(inputStream));
+            loginUser = new JSONObject(jsonTokener);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+    @Test(priority=1)
+    public void verifyInvalidUsername() {
+        utilities.log().info("This test is used to login with invalid Username and valid Password");
+        loginPage.enterUserName(loginUser.getJSONObject("invalidUser").getString("userName"));
+        loginPage.enterPassword(loginUser.getJSONObject("invalidUser").getString("password"));
+        loginPage.clickLoginBtn();
+
+        String expectedErrorMessage = stringHashMap.get("error_invalid_username_and_password");
+        String actualErrorMessage = loginPage.getErrorMessage();
+        utilities.log().info("Actual Error Message is " + actualErrorMessage + "\nExpected Error Message is " + expectedErrorMessage);
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
+
+    @Test(priority = 2)
+    public void verifyInvalidPassword() {
+        utilities.log().info("This test is used to login with valid Username and invalid Password");
+        loginPage.enterUserName(loginUser.getJSONObject("invalidPassword").getString("userName"));
+        loginPage.enterPassword(loginUser.getJSONObject("invalidPassword").getString("password"));
+        loginPage.clickLoginBtn();
+
+        String expectedErrorMessage = stringHashMap.get("error_invalid_username_and_password");
+        String actualErrorMessage = loginPage.getErrorMessage();
+
+        utilities.log().info("Actual Error Message is " + actualErrorMessage + "\nExpected Error Message is " + expectedErrorMessage);
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+
+    }
+
+    @Test(priority = 3)
+    public void verifyValidLogin() {
+        utilities.log().info("This test is used to login with valid Username and valid Password");
+        loginPage.enterUserName(loginUser.getJSONObject("validUserAndPassword").getString("userName"));
+        loginPage.enterPassword(loginUser.getJSONObject("validUserAndPassword").getString("password"));
+        productPage = loginPage.clickLoginBtn();
+
+        String expectedTitle = stringHashMap.get("product_title");
+        String actualTitle = productPage.getTitle();
+
+        utilities.log().info("Actual Product Title is " + actualTitle + "\nExpected Product Title is " + expectedTitle);
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test(priority = 4)
+    public void verifyLogout(){
+        utilities.log().info("This test is used to logout from the app");
+        productPage.clickSideNavigationBar();
+        productPage.clickLogout();
+        Assert.assertTrue(loginPage.IsFieldDisplayed(), "Logout Failed, login page username field not displayed");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        AppFactory.quitDriver();
+    }
+
 }
